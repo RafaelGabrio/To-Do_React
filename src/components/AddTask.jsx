d5f0b07a7e82dfde334665a908026b12488f3d76
Rafael Gábrio <rafael.lyra@tnpromotora.com.br>
2025-10-22 16:08:32 -0300

    Adição de borda vermelha para campos inválidos

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -5,30 +5,43 @@ function AddTask({ onAddTaskSubmit }) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
 
+  const [erros, setErros] = useState({});
+
   return (
     <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
       <Input
         type="text"
         placeholder="Digite o título da tarefa"
         value={title}
+        className={erros.title ? "focus:border-red-500 border-red-500" : ""}
         onChange={(event) => {
           setTitle(event.target.value);
         }}
+
       />
       <Input
         type="text"
         placeholder="Digite a descrição da tarefa"
         value={description}
+        className={erros.description ? "focus:border-red-500 border-red-500" : ""}
         onChange={(event) => {
           setDescription(event.target.value);
         }}
       />
       <button
         onClick={() => {
-          if (!title.trim() || !description.trim()) {
+          const isEmpty = (value) => !value.trim();
+          const newErros = {};
+
+          if (isEmpty(title)) newErros.title = true;
+          if (isEmpty(description)) newErros.description = true;
+
+          if (Object.keys(newErros).length > 0) {
+            setErros(newErros);
             return alert("Por favor, preencha todos os campos");
           }
 
+          setErros({});
           onAddTaskSubmit(title, description);
           setTitle("");
           setDescription("");
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,7 @@
-function Input(props) {
-  return (
-    <input
-      className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
-      {...props}
-    />
-  );
+function Input({ className = "", ...props }) {
+  const baseClasses = "border border-slate-300 outline-slate-400 px-4 py-2 rounded-md";
+  const finalClasses = `${className} ${baseClasses}`.trim();
+  return <input className={finalClasses} {...props} />;
 }
 
 export default Input;
